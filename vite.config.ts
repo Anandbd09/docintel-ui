0540bdb9d49c45091142036add098c6d84289cfe
Anandbd09 <anandbdundappanavar@gmail.com>
2026-06-03 17:07:50 +0530

    fixed code

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
     ssr: false,
+    spa: {
+      enabled: true,
+      prerender: {
+        outputPath: "/index",
+      },
+    },
   },
   vite: {
     server: {
