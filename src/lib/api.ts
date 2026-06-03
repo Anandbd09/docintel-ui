a32974e335817d717d9f0821c2741639bc7dd114
Anandbd09 <anandbdundappanavar@gmail.com>
2026-06-03 20:56:20 +0530

    updated codee

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const API_BASE = "http://localhost:8080";
-export const API_BASE = "";
+export const API_BASE = "http://18.212.116.23:8080";
 
 export const api = axios.create({
   baseURL: API_BASE,
